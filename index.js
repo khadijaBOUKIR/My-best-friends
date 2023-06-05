cfbfb3bba863ec3d6ecedb04897e9decbc1b3b2e
khadija <kh2007.boukir@gmail.com>
2023-06-05 10:35:57 +0100

    start

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("ajouter").addEventListener("click", function(event){
+    event.preventDefault()
+    let noun = document.getElementById("nom")
+    let nom = nom.value
+    let  = document.getElementById("prenom")
+    let  = prenom.value
+
+    console.log('nom', nom);
+    console.log('prenom', prenom);
+
+});
